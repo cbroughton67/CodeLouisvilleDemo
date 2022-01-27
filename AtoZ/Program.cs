00cd458997862e687d2928bd7d05a1e090580a3f
Chris Broughton <chris_broughton@b-f.com>
2022-01-27 10:45:16 -0500

    Added AtoZ demo

---
+++ AtoZ/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+
+namespace AtoZ
+{
+    public class InterateAlphas
+    {
+        // Print ALL alphas A-Z
+        public static void Main(string[] args)
+        {
+            PrintAtoZ();
+
+            PrintEveryOtherAtoZ();
+
+            PrintZtoA();
+
+        }
+
+        private static void PrintAtoZ()
+        {
+            string alphas = string.Empty;
+
+            for (char c = 'A'; c <= 'Z'; c++)
+            {
+                alphas = alphas + c;
+            }
+
+            Console.WriteLine(alphas);
+
+        }
+
+
+        private static void PrintEveryOtherAtoZ()
+        {
+            string alphas = string.Empty;
+
+            for (char c = 'A'; c <= 'Z'; c = Convert.ToChar(Convert.ToInt32(c) + 2))
+            {
+                alphas = alphas + c;
+            }
+
+            Console.WriteLine(alphas);
+
+        }
+
+        private static void PrintZtoA()
+        {
+            string alphas = string.Empty;
+
+            for (char c = 'Z'; c >= 'A'; c--)
+            {
+                alphas = alphas + c;
+            }
+
+            Console.WriteLine(alphas);
+
+        }
+    }
+
+}
