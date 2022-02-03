b4fa0cba38fb07544705ef036d7610f42dbf34fb
Chris Broughton <chris_broughton@b-f.com>
2022-02-03 14:37:44 -0500

    With interactive user menu!

--- AtoZ/Program.cs
+++ AtoZ/Program.cs
@@ -8,13 +8,76 @@ namespace AtoZ
         // Print ALL alphas A-Z
         public static void Main(string[] args)
         {
-            Console.WriteLine(PrintAtoZ());
+            bool doItAgain = true;
 
-            Console.WriteLine(PrintEveryOtherAtoZ());
+            do
+            {
 
-            Console.WriteLine(PrintZtoA());
+                string selection = CreateMenu();
 
+                switch (selection)
+                {
+                    case "1":
+                        Console.WriteLine("\n" + PrintAtoZ());
+                        doItAgain = true;
+                        break;
+
+                    case "2":
+                        Console.WriteLine("\n" + PrintEveryOtherAtoZ());
+                        doItAgain = true;
+                        break;
+
+                    case "3":
+                        Console.WriteLine("\n" + PrintZtoA());
+                        doItAgain = true;
+                        break;
+
+                    case "X": case "x":
+                         doItAgain = false;
+                        break;
+
+                    default:
+                        doItAgain = true;
+                        break;
+                }
+
+                if (doItAgain == true)
+                {   Console.WriteLine("Press a key to continue...");
+                    Console.ReadKey();
+                }
+
+            }
+            while (doItAgain == true);
+            
+         }
+
+        public static string CreateMenu()
+        {
+            string valueEntered;
+
+            do   //do until valid menu selection entered
+            {
+                Console.Clear();
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1) Display Alphabet A-Z");
+                Console.WriteLine("2) Skip a number of letters in A-Z");
+                Console.WriteLine("3) Display Alphabet In Reverse Z-A");
+                Console.WriteLine("X) Exit Program");
+                Console.Write("\r\nSelect an option: ");
+
+                valueEntered = Convert.ToString(Console.ReadKey().KeyChar);
+            }
+            while (valueEntered != "1" &&
+                   valueEntered != "2" &&
+                   valueEntered != "3" &&
+                   valueEntered != "X" &&
+                   valueEntered != "x");
+
+            return valueEntered;
         }
+  
+            
+     
 
         private static string PrintAtoZ()
         {
@@ -38,8 +101,9 @@ namespace AtoZ
 
             do   //Ask for number until a number is entered
             {
+                Console.Clear();
                 Console.WriteLine("Enter a number: ");
-                valueEntered = Convert.ToString(Console.ReadLine());
+                valueEntered = Convert.ToString(Console.ReadKey().KeyChar);
             }
             while (int.TryParse(valueEntered, out skip) == false);
 
